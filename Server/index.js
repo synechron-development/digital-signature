91ec96e111b0aa76a2996a35ce68be92c6bac4b5
Moataz <wesiy@gmail.com>
2018-08-26 17:41:29 +0400

    [add] node server socket

--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,57 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var cors = require('cors')
 
-app.get('/', function(req, res){
+app.use(cors())
+
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('draw', function(data){
+io.on('connection', function (socket) {
+  socket.on('draw', function (data) {
     io.emit('draw', data);
   });
 
-  socket.on('clear', function(){
+  socket.on('clear', function () {
     io.emit('clear');
   });
 });
 
-http.listen(4000, function(){
+http.listen(4000, function () {
   console.log('Digital Signature Server Running on http://10.85.30.133:4000');
+});
+
+
+
+
+
+
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/message', function (req, res) {
+  var number = req.body.number;
+  var loc = req.body.loc;
+
+
+// Twilio Credentials
+const accountSid = 'AC18222690fa4ea24ffa00d60d807bea68';
+const authToken = '41f841c1e875d833b4781445e0cb285b';
+const client = require('twilio')(accountSid, authToken);
+console.log('sending message')
+client.messages.create({
+    to: '+971507021612',
+    from: '+8049252205',
+    body: "Click here to sign",
+    mediaUrl: loc,
+  })
+  .then((message) => {console.log('message sent' + message.sid);res.end("yes");});
+
+
+
+  
 });
--- UI/src/componenets/SignPad/SignPad.js
+++ UI/src/componenets/SignPad/SignPad.js
@@ -2,37 +2,44 @@ import React, { Component } from 'react'
 import './SignPad.css';
 import SignaturePad from 'signature_pad';
 import openSocket from 'socket.io-client';
+import axios from 'axios';
 
-
+const pnumber  = "+971507021612"
 const socket = openSocket('http://10.85.30.133:4000');
 
 class SignPad extends Component {
   constructor(props) {
     super();
-    
+
 
     if ("ontouchstart" in document.documentElement) {
       this.state = {
-        isTouch: "true"
+        isTouch: true,
+        messageSent: false
       }
     }
-    else{
+    else {
       this.state = {
-        isTouch: "false"
+        isTouch: false,
+        messageSent: false
       }
     }
-
-    
-    
-      
-    
-    
-
-
   }
+
   componentDidMount() {
     var canvas = document.getElementById('signature-pad');
-    
+
+
+    // Initialize Signature Pad
+    const signaturePad = new SignaturePad(canvas, {
+      backgroundColor: 'rgb(255, 255, 255)', // necessary for saving image as JPEG; can be removed is only saving as PNG or SVG
+      onEnd: function (a, b) {
+        //console.log(a, b);
+        const data = signaturePad.toData();
+        socket.emit('draw', data);
+      }
+    });
+
     function resizeCanvas() {
       // When zoomed out to less than 100%, for some very strange reason,
       // some browsers report devicePixelRatio as less than 1
@@ -42,44 +49,73 @@ class SignPad extends Component {
       canvas.height = canvas.offsetHeight * ratio;
       canvas.getContext("2d").scale(ratio, ratio);
 
+      // Broadcast Clear Canvas Event
       socket.emit('clear');
     }
 
     
-    const signaturePad = new SignaturePad(canvas, {
-      backgroundColor: 'rgb(255, 255, 255)', // necessary for saving image as JPEG; can be removed is only saving as PNG or SVG
-      onEnd: function (a, b) {
-        //console.log(a, b);
-        const data = signaturePad.toData();
-        socket.emit('draw', data);
-      }
-    });
     
-  window.onresize = resizeCanvas;
-  setTimeout(()=>{
     resizeCanvas();
-  },500);
-    
+    window.onresize = resizeCanvas;
 
-  // Setup Socket Events
-  socket.on('draw', (msg)=>{
-    //signaturePad.clear();
-    signaturePad.fromData(msg);
-  });
-  socket.on('clear',()=>{
-    signaturePad.clear();
-  });
 
+    // Setup Socket Events
+    socket.on('draw', (msg) => {
+      //signaturePad.clear();
+      signaturePad.fromData(msg);
+    });
+    socket.on('clear', () => {
+      signaturePad.clear();
+    });
+
+    if (this.state.isTouch === false) {
+      signaturePad.off();
+    }
 
   }
+
+  clearCanvas(){
+   socket.emit('clear');
+  }
+
+  sendMessage = ()=>{
+    console.log('send');
+    this.setState({
+        messageSent: true
+    });
+
+    axios.post('http://localhost:4000/message', {
+      number: pnumber,
+      loc: window.location.href
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
   render() {
     return (
-      <div className="uk-placeholder signature-pad">
-        <div className="wrapper">
-          <canvas id="signature-pad" width="300" height="200"></canvas>
+      <div className="uk-placeholder signature-pad uk-position-relative">
+        <div className="uk-grid-small uk-flex-center" data-uk-grid>
+          <div className="uk-width-2-3@s">
+            <div className="wrapper">
+              <canvas id="signature-pad" width="300" height="200"></canvas>
+            </div>
+          </div>
+          <div className="uk-width-1-3@s">
+            <button onClick={this.clearCanvas} className="uk-button uk-button-default">Clear</button>
+            
+          </div>
         </div>
 
-        
+          {!this.state.isTouch && !this.state.messageSent && <div className="uk-overlay-default uk-position-cover">
+                <div className="uk-position-center">
+                <button onClick={this.sendMessage} className="uk-button uk-button-default">Sign on my phone <i data-uk-icon="phone"></i></button>
+                </div>
+            </div>}
       </div>
 
     )
